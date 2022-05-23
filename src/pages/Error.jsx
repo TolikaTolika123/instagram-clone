26cfca55a064050a5d9d7e38150f44f3e4ca992d
TolikaTolika123 <tolikatolika794@gmail.com>
2022-05-23 13:54:58 +0400

    added error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='error'>
+      <div className="container">
+      <h2 className="error-title">Sorry, this page isn't available.</h2>
+      <p className="error-subtitle">
+        <span>The link you followed may be broken, or the page may have been removed. </span>
+        <Link to='/'>Go back to Instagram.</Link>
+      </p>
+      </div>     
+    </div>
   )
 }
 
