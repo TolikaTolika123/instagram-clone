08cc347a5c296d76b41d73af8adb197e7aa4249a
TolikaTolika123 <tolikatolika794@gmail.com>
2022-06-03 14:23:57 +0400

    added user searching functionale

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import Navbar from './Navbar';
 
 import instagramImg from '../images/instagram.png'
 import loupeImg from '../images/loupe.svg'
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 
-const Header = ({dropdownVisible, setDropdownVisible}) => {
-  
+const Header = ({ dropdownVisible, setDropdownVisible }) => {
+  const [usersList, setUsersList] = useState([])
+  const [searchedUsers, setSearchedUsers] = useState([])
+  const [searchedValue, setsearchedValue] = useState('')
+  const [searchDropdownVisible, setSearchDropdownVisible] = useState(false)
+
+  const changeSearchedUsers = e => {
+    setsearchedValue(e.target.value);
+    if (e.target.value === '') {
+      setSearchedUsers([])
+      setSearchDropdownVisible(false)
+    } else {
+      setSearchedUsers(usersList.filter(user => user.username.toLowerCase().includes(e.target.value.toLowerCase())))
+      setSearchDropdownVisible(true)
+    }
+  }
+
+  const loadUsers = async () => {
+    const querySnapshot = await getDocs(collection(getFirestore(), "users"));
+    querySnapshot.forEach((doc) => {
+      const user = doc.data();
+      if (!usersList.find(user => user.id === doc.id)) {
+        const newUser = { id: doc.id, ...user };
+        const prevUsers = usersList;
+        prevUsers.push(newUser)
+        setUsersList(structuredClone(prevUsers))
+      }
+    });
+  }
+
+  useEffect(() => {
+    loadUsers();
+  }, [])
+
+
   return (
     <header className='header'>
       <div className="container">
@@ -17,16 +51,32 @@ const Header = ({dropdownVisible, setDropdownVisible}) => {
             <label htmlFor="header__serach-input">
               <img src={loupeImg} alt="" />
             </label>
-            <input placeholder='Search' type="text" className="header__search-input" id="header__search-input" list='header__search-results' />
-            <datalist id="header__search-results">
-              <option value="Edge" />
-              <option value="Firefox" />
-              <option value="Chrome" />
-              <option value="Opera" />
-              <option value="Safari" />
-            </datalist>
+            <input
+              placeholder='Search'
+              type="text"
+              className="header__search-input"
+              value={searchedValue}
+              onChange={changeSearchedUsers}
+            />
+            <div className={searchDropdownVisible ? "header__search-dropdown active" : "header__search-dropdown"}>
+            <ul className="header__search-list">
+              {searchedUsers.map(user => (
+                <li className='header__search-item' key={user.id}>
+                  <Link className='header__search-itemLink' to={`/profile/${user.username}`}>
+                    <div className="header__search-itemImg">
+                      <img src={user.publicImageUrl} alt="" />
+                    </div>
+                    <div className="header__search-itemContent">
+                      <p className="header__search-itemUsername">{user.username}</p>
+                      <p className="header__search-itemFullName">{user.fullName}</p>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            </div>
           </div>
-          <Navbar {...{dropdownVisible, setDropdownVisible}}/>
+          <Navbar {...{ dropdownVisible, setDropdownVisible }} />
         </div>
       </div>
     </header>
